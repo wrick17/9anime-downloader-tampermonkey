e05fbca46f82e368e2fa7b4864353c187e4f3c6c
Pratyush Poddar <wrick17@gmail.com>
2017-07-27 00:20:07 +0530

    added a little bit of sanity

--- 9anime Batch Downloader.user.js
+++ 9anime Batch Downloader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         9anime Batch Downloader
 // @namespace    https://greasyfork.org/en/scripts/31118-9anime-batch-downloader
-// @version      2.1
+// @version      2.2
 // @description  Download all episodes automatically from Server F4 (only). Copy the links and add to IDM as a batch, and enjoy!!
 // @author       wrick17
 // @match        https://9anime.to/watch/*
@@ -19,7 +19,7 @@
         clearInterval(interval);
         $('#servers')
         .before('<button class="btn btn-primary btn-sm download-trigger" style="margin-bottom: 10px;">Download all of \'em!</button><div class="download-label" style="display: none; color: #9a9a9a; margin-bottom: 15px; ">Add the following links to your download manager and enjoy:</div>')
-        .before('<textarea class="download-links" style="display: none; width: 100%; height: 200px; background: #0f0e13; color: #9a9a9a; border: none; margin-bottom: 10px;"></textarea>');
+        .before('<textarea class="download-links" style="display: none; width: 100%; height: 200px; background: #0f0e13; color: #9a9a9a; border: none; margin-bottom: 10px; padding: 10px;"></textarea>');
       }
     }, 100);
 
